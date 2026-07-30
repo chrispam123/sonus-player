86620fd597a76775df816d72ff4888b7ca4f46cc
chrispam123 <uopechris@gmail.com>
2026-07-30 10:53:25 +0200

    release: v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.sonus.player"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
