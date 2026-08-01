3fdba327a74766688e1aef4af5daa4d18a10425b
chrispam123 <uopechris@gmail.com>
2026-08-01 15:06:19 +0200

    release: v1.0.5- API KEY +Throttling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "com.sonus.player"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
